2f44d6747fcab2d0eecc67a990b65f3fe2d740dd
Denilson Santos <denilson.avsantos@hotmail.com>
2020-04-13 16:13:59 -0300

    Estilizando header continuação

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -8,4 +8,25 @@ export const Container = styled.header`
   margin: 50px 0;
 `;
 
-export const Cart = styled(Link)``;
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+  div {
+    text-align: right;
+    margin: 10px;
+  }
+  strong {
+    display: block;
+    color: #fff;
+  }
+  span {
+    font-size: 12px;
+    color: #999;
+  }
+`;
