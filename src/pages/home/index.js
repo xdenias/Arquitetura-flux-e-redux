f09158f31124ae50f53c0ece7bfb741c5c7eee8a
Denilson Santos <denilson.avsantos@hotmail.com>
2020-04-13 17:27:41 -0300

    Estilizando home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Hello, Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,99</span>
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#FFF" />3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
