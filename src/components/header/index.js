592c78fd2434dc5b2ebfe89a9b0e602b20e1180a
Denilson Santos <denilson.avsantos@hotmail.com>
2020-04-13 16:01:46 -0300

    Estilizando header

---
+++ src/components/header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Cart } from './styles';
+import { MdShoppingBasket } from 'react-icons/md';
+import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="RocketShoes"></img>
+      </Link>
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+}
---
+++ src/components/header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+`;
+
+export const Cart = styled(Link)``;
