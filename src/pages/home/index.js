ca71893ef6827e8cd53ddee3af4edabb2c25b33a
Denilson Santos <denilson.avsantos@hotmail.com>
2020-04-22 12:02:36 -0300

    Configurando redux

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,101 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,99</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" />3
-          </div>
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+  async componentDidMount() {
+    const response = await api.get('products');
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,99</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" />3
-          </div>
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,99</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" />3
-          </div>
+    this.setState({ products: data });
+  }
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>Tênis muito legal</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button">
+              <div>
+                <MdShoppingCart size={16} color="#FFF" />3
+              </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,99</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,99</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-alphabounce-run-parley-adidas/06/COL-8059-006/COL-8059-006_zoom2.jpg?ts=1563179125&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis muito legal</strong>
-        <span>R$129,99</span>
-        <button type="button">
-          <div>
-            <MdShoppingCart size={16} color="#FFF" />3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
